897c307ebd0b033b62b76132ae7776bd09d2683e
cuonggaim <cuonggaim@gmail.com>
2022-03-13 21:06:48 +0700

    aloha

--- video3/video3.js
+++
@@ -1,8 +0,0 @@
-console.log('Chao Cuong')
-let c = 5
-let d = 5.9
-let x = 2
-let y = 3
-let z = 4
-console.log(`hihi'haha'"'"`)
-console.log(`Hien x y z`,'x=',x,"y=",y,`z=`,z)
---
+++ video4/video4.js
@@ -0,0 +1,8 @@
+let a = `Tôi là chuỗi`
+console.log(a)
+console.log(4 + 5 + 'a')
+console.log(`a` + 4 + 5)
+let obj = { name: `Cuong`,
+            address: `Vinh`};
+console.log(`Ten ban la gi?`,`Ten toi la`,obj.name)
+console.log(`Ban o dau?`,`Toi o`, obj.address)
